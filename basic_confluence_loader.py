86d8c15d6767685a9549f31032ad99b0832f5ccd
Yash Kulkarni <yash.kulkarni@salesforce.com>
2023-06-23 12:39:51 +0530

    Debug - confluence loader

--- basic_confluence_loader.py
+++ basic_confluence_loader.py
@@ -4,3 +4,4 @@ loader = ConfluenceLoader(
     url="https://confluence.internal.salesforce.com", username="yash.kulkarni", api_key="apiTokenAuthentication", confluence_kwargs={"verify_ssl": False}
 )
 documents = loader.load(space_key="CMEKB", include_attachments=True, limit=50)
+print(f'Debug:: {len(documents)} documents loaded successfully.')
